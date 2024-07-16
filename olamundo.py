4930c54749347e0fb83ddd82b34c650e4fd24ec4
francis_bowe <franciscodomingos20155@gmail.com>
2024-07-17 00:26:55 +0100

    Ola mundo em Python

---
+++ olamundo.py
@@ -0,0 +1,11 @@
+'''
+Author: francis_bowe franciscodomingos20155@gmail.com
+Date: 2024-07-17 00:16:45
+LastEditors: francis_bowe franciscodomingos20155@gmail.com
+LastEditTime: 2024-07-17 00:21:02
+FilePath: olamundo.py
+Description: Meu primeiro programa em python
+'''
+
+var = "HelloWorld"
+print(var)
